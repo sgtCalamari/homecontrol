3458f55d00f4f74c110bd8a1ee12d90a2a5de613
Joe Martin <throwstuff@joemart.in>
2021-09-27 18:23:38 -0400

    Abstract api url
    - Referencing .env files

--- client/src/App.js
+++ client/src/App.js
@@ -18,19 +18,19 @@ class App extends React.Component {
   }
 
   getRooms() {
-    axios.get('http://localhost:4000/rooms')
+    axios.get(`${process.env.REACT_APP_API_URL}/rooms`)
       .then(r => this.setState({rooms: r.data}))
       .catch(err => console.log(err));
   }
 
   getDevices() {
-    axios.get('http://localhost:4000/devices')
+    axios.get(`${process.env.REACT_APP_API_URL}/devices`)
       .then(d => this.setState({devices: d.data.filter(d => d.isLight)}))
       .catch(err => console.log(err));
   }
 
   switch(action, deviceId) {
-    axios.post(`http://localhost:4000/devices/${deviceId}/${action}`)
+    axios.post(`${process.env.REACT_APP_API_URL}/devices/${deviceId}/${action}`)
       .then(r => console.log(r))
       .catch(err => console.log(err));
   }
@@ -41,6 +41,8 @@ class App extends React.Component {
     return (
       <div className="App">
         <div>
+          <h1>Home Control</h1>
+          <hr/>
           {rooms.map(r => (
             <>
             <h2 key={r.roomId}>{r.name}</h2>
